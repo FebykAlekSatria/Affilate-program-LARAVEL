49f89746a23e6e136b76eeee241d01d79b31b0c1
stringsout <76243098+FebykAlekSatria@users.noreply.github.com>
2021-12-07 11:59:38 +0700

    Routing

---
+++ affilate-program/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>ini halaman home aku yang mana</h1>
+
+<div class="container ">
+    @include('partials.card')
+</div>
+@endsection
---
+++ affilate-program/resources/views/post.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>ini halaman post</h1>
+
+@endsection
